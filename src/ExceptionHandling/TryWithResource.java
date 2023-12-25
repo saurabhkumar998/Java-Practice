61b12d4fbf39365df475274789cf095e6c64ad20
saurabh <saurabh.kumar.2382000@gmail.com>
2023-12-25 21:10:12 +0530

    learnt some file methods

--- src/ExceptionHandling/TryWithResource.java
+++ src/ExceptionHandling/TryWithResource.java
@@ -1,19 +1,32 @@
 package ExceptionHandling;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.sql.SQLOutput;
+import java.io.*;
 
 public class TryWithResource {
     public static void main(String [] args) {
-        String filename = "testing.txt";
+        String filename = "src/files/testing.txt";
         testFile(filename);
+
+        System.out.println("Current Working Directory (cwd) : "+ new File("").getAbsolutePath());
+
+        for(File f : File.listRoots()) {
+            System.out.println(f);
+        }
+        // checking file permissions
+        System.out.println("Checking execute permission : " + new File(filename).canExecute());
+        System.out.println("Checking read permission : " + new File(filename).canRead());
+        System.out.println("Checking write permission : " + new File(filename).canWrite());
     }
 
     public static void testFile(String filename) {
 
-        try(FileReader reader = new FileReader(filename)) {
+        try(FileReader fileReader = new FileReader(filename); BufferedReader reader = new BufferedReader(fileReader)) {
+
+            String line;
+
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
 
         }catch(FileNotFoundException e) {
             System.out.println("The file "+ filename + " doesn't exist.");
